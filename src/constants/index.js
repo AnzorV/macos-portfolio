a9e6a0f588f38733340b4e20d9e80bcf8e7a9276
AnzorV <anzorv@gmail.com>
2025-12-15 20:32:59 +0000

    finalized portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -683,7 +683,7 @@ const ABOUT_LOCATION = {
             kind: "file",
             fileType: "img",
             position: "top-52 left-80",
-            imageUrl: "/images/amir-3.jpeg",
+            imageUrl: "/images/amir-3.jpg",
         },
         {
             id: 4,
